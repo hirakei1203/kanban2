1833dce088550a82f287b95731714ec6fd28a7d4
hirakei1203 <56023847+hirakei1203@users.noreply.github.com>
2019-12-20 23:21:29 +0900

    index3

--- kanban/views.py
+++ kanban/views.py
@@ -2,8 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-from django.http import HttpResponse
-
-
 def index(request):
     return render(request, "kanban/index.html") 
